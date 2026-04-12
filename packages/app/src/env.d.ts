985cb6a3ae3dd0cfd9e43b2dff9a07cbc19ce444
Dima <dima@inkeep.com>
2026-04-12 23:55:44 +0700

    fix

--- packages/app/src/env.d.ts
+++ packages/app/src/env.d.ts
@@ -1,6 +1,6 @@
 declare namespace globalThis {
   import type { HocuspocusProvider } from '@hocuspocus/provider';
   import type { ProviderPool } from '@/editor/provider-pool';
-  var __activeProvider: HocuspocusProvider;
-  var __providerPool: ProviderPool | null;
+  var __providerPool: ProviderPool;
+  var __activeProvider: HocuspocusProvider | null;
 }
