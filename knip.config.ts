98f9207b2d9f161a9c835d97d41b389b216ea8ad
Dima <dima@inkeep.com>
2026-04-14 01:48:52 +0700

    rm

--- knip.config.ts
+++ knip.config.ts
@@ -3,11 +3,12 @@ import type { KnipConfig } from 'knip';
 export default {
   tags: ['-lintignore'],
   ignoreDependencies: [],
+  ignoreIssues: {
+    'packages/app/src/components/ui/*': ['exports'],
+    'docs/source.config.ts': ['exports'],
+  },
   workspaces: {
     'packages/app': {
-      ignoreIssues: {
-        'src/components/ui/*': ['exports'],
-      },
     },
   },
 } satisfies KnipConfig;
